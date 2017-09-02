04c5635d15131ea1f26ce7e85cfee59d8489d03b
mjamesruggiero <mjamesruggiero@gmail.com>
2017-09-01 22:21:18 -0700

    Helper function echoes available routes

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 from veronica import app
+from flask import url_for
 from flask.ext.script import Manager
 
 manager = Manager(app)
@@ -14,6 +15,25 @@ def test():
 
     if not result.wasSuccessful():
         sys.exit(1)
+
+@manager.command
+def list_routes():
+    import urllib
+    output = []
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        output.append(line)
+
+    for line in sorted(output):
+        print line
+
 
 
 if __name__ == "__main__":
